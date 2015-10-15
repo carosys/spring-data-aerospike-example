57f067eb47e264431e695fb24904e9996025d2b3
jean <jean.mercier@carosys.com>
2015-10-15 16:52:12 -0400

    Example

---
+++ spring-data-aerospike-example/src/main/java/example/springdata/aerospike/config/TestRepositoryConfig.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package example.springdata.aerospike.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.aerospike.core.AerospikeTemplate;
+import org.springframework.data.aerospike.repository.config.EnableAerospikeRepositories;
+
+import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.policy.ClientPolicy;
+
+/**
+ *
+ *
+ * @author Peter Milne
+ * @author Jean Mercier
+ *
+ */
+@Configuration
+@EnableAerospikeRepositories(basePackages = "example.springdata.aerospike")
+public class TestRepositoryConfig {
+	public @Bean(destroyMethod = "close") AerospikeClient aerospikeClient() {
+
+		ClientPolicy policy = new ClientPolicy();
+		policy.failIfNotConnected = true;
+
+		return new AerospikeClient(policy, "52.23.205.208", 3000);
+	}
+
+	public @Bean AerospikeTemplate aerospikeTemplate() {
+		return new AerospikeTemplate(aerospikeClient(), "test");
+	}
+
+}
